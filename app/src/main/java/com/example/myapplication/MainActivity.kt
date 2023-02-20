3e21680b764944a55aecde6c9676b782b8a21caa
samermushtaha <samermushtaha0@gmail.com>
2023-02-20 11:12:45 +0200

    new branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // new branch
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
